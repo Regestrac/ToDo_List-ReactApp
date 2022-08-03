657cf834658e7c82511ec2310e3bc955b1588512
Sojo C Johny <sojocjohny37@gmail.com>
2022-08-03 16:31:31 +0530

    Added a basic layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="">
-      Hello World
+    <div className="app">
+      <div className='mainHeading'>
+        <h1>ToDo List</h1>
+      </div>
+      <div className='subHeading'>
+        <h2>Hello there, Have a nice day!</h2>
+      </div>
+      <div className='input'>
+        <input  type="text" placeholder='🖋️ Add item...'></input>
+        <i  className="fas fa-plus"></i>
+      </div>
     </div>
   );
 }
