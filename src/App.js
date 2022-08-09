20e2530fe9be619ad11c381d6f6cdb3095eeaafb
Sojo C Johny <sojocjohny37@gmail.com>
2022-08-09 22:10:07 +0530

    Modified input feild design

--- src/App.js
+++ src/App.js
@@ -3,18 +3,93 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [toDos, setToDos] = useState([])
+  const [toDo, setToDo] = useState('')
+  const today=new Date()
+  const dayNum=today.getDay()
+  const dayList=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+  const day=dayList[dayNum]
+  const handleInput=((e)=>{
+    e.preventDefault();
+    if(toDo){
+      setToDos([...toDos,{
+        id:Date.now(),
+        text:toDo,
+        statusCompleted:false,
+        statusRemoved:false
+      }]);
+      setToDo('');
+    }
+  })
+  const clearInput=()=>{
+    setToDo('');
+  }
 
   return (
     <div className="app">
-      <div className='mainHeading'>
-        <h1>ToDo List</h1>
+      <div className='headings'>
+        <div className='mainHeading'>
+          <h1>ToDo List</h1>
+        </div>
+        <div className='subHeading'>
+          <h2>Hey its {day}.</h2>
+        </div>
       </div>
-      <div className='subHeading'>
-        <h2>Hello there, Have a nice day!</h2>
-      </div>
-      <div className='input'>
-        <input  type="text" placeholder='🖋️ Add item...'></input>
-        <i  className="fas fa-plus"></i>
+        <form onSubmit={handleInput}>
+          <div className='inputOptions'>
+          <div className='left'>
+            <input value={toDo} onChange={(e) => setToDo(e.target.value)} type="text" placeholder='🖋️ Plan Something...'></input>
+          </div>
+          <div className='middle'>
+            <i onClick={clearInput} className="fas fa-eraser" title="Clear"></i>
+          </div>
+          <div className='right'>
+            <i type='submit' onClick={handleInput} className="fa-solid fa-circle-plus" title="Add"></i>
+            {/* onClick={() => setToDos([...toDos, {id:Date.now(),text:toDo, status:false}])} */}
+          </div>
+          </div>
+        </form>
+
+      <div className="todos">
+        <h3>Completed</h3>
+        {toDos && toDos.map((obj) => {
+          if (obj.statusCompleted && !obj.statusRemoved) {
+            return (
+              <div className="todo">
+                <div className="left">
+                  <input onChange={(e) => {
+                    setToDos(toDos.filter(obj2 => {
+                      if (obj2.id === obj.id) {
+                        obj2.status = e.target.checked
+                      }
+                      return obj2;
+                    }))
+                  }} value={obj.status} type="checkbox" />
+                  <p>{obj.text}</p>
+                </div>
+                <div className="right">
+                  <i className="fas fa-times"></i>
+                </div>
+              </div>
+            )
+          }
+        })
+        }
+        {toDos.map((obj)=>{
+          if(obj.status){
+            return(
+              <div className='todo'>
+                <div className='left'>
+                <p>{obj.text}</p>
+                </div>
+                <div className='right'>
+                <i className='fa-solid fa-trash-can'></i>
+                </div>
+              </div>
+            )
+          }
+          return null;
+        })}
       </div>
     </div>
   );
